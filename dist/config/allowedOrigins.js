66cae002ef6e887def3d0f5091932728619f2857
Mordechai1323 <112948082+Mordechai1323@users.noreply.github.com>
2023-07-22 05:27:10 +0300

    remove dist

--- dist/config/allowedOrigins.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.allowedOrigins = void 0;
-exports.allowedOrigins = ['http://localhost:3000', 'https://team-up00.netlify.app'];
--- dist/routes/login.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const express_1 = __importDefault(require("express"));
-const router = express_1.default.Router();
-const loginController_1 = __importDefault(require("../controllers/users/loginController"));
-router.post('/', loginController_1.default.handleLogin);
-module.exports = router;
