a0ccd8d7e0038e58e4f2ff5b9c1fdf77cb769d39
Mordechai1323 <112948082+Mordechai1323@users.noreply.github.com>
2023-08-24 13:11:45 +0300

    Added a test for deleteGroup function.

--- src/tests/boards/groups.test.ts
+++ src/tests/boards/groups.test.ts
@@ -58,4 +58,47 @@ describe('groups tests', () => {
       expect(response.body).toHaveProperty('err', 'fail validating token');
     });
   });
+
+  describe('delete board test', () => {
+    let token: string;
+    let groupID: string;
+    beforeAll(async () => {
+      await UserModel.deleteMany({});
+      await BoardModel.deleteMany({});
+      const response = await request(app).post('/register').send(testUser);
+      token = response.body.accessToken;
+      const resBoard = await request(app).post('/boards').set('authorization', `Bearer ${token}`).send({ name: 'board test' });
+      const boardID = resBoard.body._id
+      const resAddGroup = await request(app).post('/groups').set('authorization', `Bearer ${token}`).query({ boardID });
+      groupID = resAddGroup?.body._id;
+    });
+
+    test('Should return 200 if board delete successfully', async () => {
+      const response = await request(app).delete(`/groups`).set('authorization', `Bearer ${token}`).query({ groupID });
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.body).toHaveProperty('deletedCount', 1);
+    });
+
+    test('Should return 400 if not provided groupID', async () => {
+      const response = await request(app).delete(`/groups`).set('authorization', `Bearer ${token}`);
+
+      expect(response.statusCode).toEqual(400);
+    });
+
+    test('Should return 401 if token is missing', async () => {
+      const response = await request(app).delete(`/groups`).query({ groupID });
+
+      expect(response.statusCode).toEqual(401);
+      expect(response.body).toHaveProperty('err', 'authentication missing');
+    });
+
+    test('Should return 403 if token invalid', async () => {
+      const response = await request(app).delete(`/groups`).set('authorization', `Bearer invalidToken`).query({ groupID });
+
+      expect(response.statusCode).toEqual(403);
+      expect(response.body).toHaveProperty('err', 'fail validating token');
+    });
+  });
+
 });
