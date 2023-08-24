996d0fe5f72fbb2c8ee734d037ed89f3b7ff4e92
Mordechai1323 <112948082+Mordechai1323@users.noreply.github.com>
2023-08-24 13:25:52 +0300

    Added a test for getGroups function.

--- src/tests/boards/groups.test.ts
+++ src/tests/boards/groups.test.ts
@@ -68,7 +68,7 @@ describe('groups tests', () => {
       const response = await request(app).post('/register').send(testUser);
       token = response.body.accessToken;
       const resBoard = await request(app).post('/boards').set('authorization', `Bearer ${token}`).send({ name: 'board test' });
-      const boardID = resBoard.body._id
+      const boardID = resBoard.body._id;
       const resAddGroup = await request(app).post('/groups').set('authorization', `Bearer ${token}`).query({ boardID });
       groupID = resAddGroup?.body._id;
     });
@@ -101,4 +101,53 @@ describe('groups tests', () => {
     });
   });
 
+  describe('get my groups test', () => {
+    let token: string;
+    let boardID: string;
+    beforeAll(async () => {
+      await UserModel.deleteMany({});
+      await BoardModel.deleteMany({});
+      const response = await request(app).post('/register').send(testUser);
+      token = response.body.accessToken;
+      const resBoard = await request(app).post('/boards').set('authorization', `Bearer ${token}`).send({ name: 'board test' });
+      boardID = resBoard.body._id;
+      await request(app).post('/groups').set('authorization', `Bearer ${token}`).query({ boardID });
+    });
+
+    test('Should return array of groups', async () => {
+      const response = await request(app).get(`/groups`).set('authorization', `Bearer ${token}`).query({ boardID });
+
+      expect(response.status).toEqual(200);
+      expect(Array.isArray(response.body)).toBe(true);
+      expect(response.body.length).toBe(2);
+    });
+
+    test('Should return only one group', async () => {
+      const response = await request(app).get(`/groups`).set('authorization', `Bearer ${token}`).query({ boardID, s: 'New Group' });
+
+      expect(response.status).toEqual(200);
+      expect(Array.isArray(response.body)).toBe(true);
+      expect(response.body.length).toBe(1);
+    });
+
+    test('Should return 400 if not provided boardID', async () => {
+      const response = await request(app).get(`/groups`).set('authorization', `Bearer ${token}`);
+
+      expect(response.statusCode).toEqual(400);
+    });
+
+    test('Should return 401 if token is missing', async () => {
+      const response = await request(app).get(`/groups`).query({ boardID });
+
+      expect(response.statusCode).toEqual(401);
+      expect(response.body).toHaveProperty('err', 'authentication missing');
+    });
+
+    test('Should return 403 if token invalid', async () => {
+      const response = await request(app).get(`/groups`).set('authorization', `Bearer invalidToken`).query({ boardID });
+
+      expect(response.statusCode).toEqual(403);
+      expect(response.body).toHaveProperty('err', 'fail validating token');
+    });
+  });
 });
