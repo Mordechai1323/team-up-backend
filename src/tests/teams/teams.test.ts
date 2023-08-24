baec112f4c5abe5daa170c05862455b626c5dd84
Mordechai1323 <112948082+Mordechai1323@users.noreply.github.com>
2023-08-24 17:49:14 +0300

    Added a test for attTeamMember function.

--- src/tests/teams/teams.test.ts
+++ src/tests/teams/teams.test.ts
@@ -82,7 +82,7 @@ describe('teams tests', () => {
     });
   });
 
-  describe('delete team ', () => {
+  describe('delete team', () => {
     let token: string;
     let user: any;
     beforeAll(async () => {
@@ -113,7 +113,7 @@ describe('teams tests', () => {
     });
   });
 
-  describe('get team ', () => {
+  describe('get team test', () => {
     let token: string;
     beforeAll(async () => {
       await UserModel.deleteMany({});
@@ -128,7 +128,7 @@ describe('teams tests', () => {
       await request(app).post('/teams').set('authorization', `Bearer ${token}`).send(team);
     });
 
-    test('Should return 200 and ', async () => {
+    test('Should return 200 and team', async () => {
       const response = await request(app).get('/teams').set('authorization', `Bearer ${token}`);
 
       expect(response.status).toEqual(200);
@@ -137,9 +137,71 @@ describe('teams tests', () => {
       expect(response.body.teamMembers[1].name).toEqual('test1');
     });
 
-    test('Should return 400 if some or all of the team members were not found', async () => {
-      await TeamModel.deleteMany({})
-      const response = await request(app).get('/teams').set('authorization', `Bearer ${token}`)
+    test('Should return 400 if team not exist', async () => {
+      await TeamModel.deleteMany({});
+      const response = await request(app).get('/teams').set('authorization', `Bearer ${token}`);
+
+      expect(response.status).toEqual(400);
+    });
+  });
+
+  describe('add team member test', () => {
+    let token: string;
+    beforeEach(async () => {
+      await UserModel.deleteMany({});
+      await BoardModel.deleteMany({});
+      await TeamModel.deleteMany({});
+      const resUer = await request(app).post('/register').send(testUser);
+      const resUer1 = await request(app).post('/register').send(test1User);
+      const user3 = { name: 'test3', email: 'test3@mail.com', password: '******' };
+      await request(app).post('/register').send(user3);
+      token = resUer.body.accessToken;
+      const token1 = resUer1.body.accessToken;
+      await request(app).post('/boards').set('authorization', `Bearer ${token}`).send({ name: 'board test' });
+      await request(app).post('/boards').set('authorization', `Bearer ${token1}`).send({ name: 'board test1' });
+      await request(app).post('/teams').set('authorization', `Bearer ${token}`).send(team);
+    });
+
+    test('Should return 200 and added the user', async () => {
+      const response = await request(app)
+        .post('/teams/addTeamMember')
+        .set('authorization', `Bearer ${token}`)
+        .send({ team_member: 'test3@mail.com' });
+
+      expect(response.status).toEqual(200);
+      expect(response.body.team_members.length).toEqual(3);
+    });
+
+    test('Should return 400 if team member is missing', async () => {
+      const response = await request(app).post('/teams/addTeamMember').set('authorization', `Bearer ${token}`);
+
+      expect(response.statusCode).toEqual(400);
+      expect(response.body).toEqual([
+        {
+          message: '"team_member" is required',
+          path: ['team_member'],
+          type: 'any.required',
+          context: { label: 'team_member', key: 'team_member' },
+        },
+      ]);
+    });
+
+    test('Should return 400 if user not exist', async () => {
+      await UserModel.deleteMany({});
+      const response = await request(app)
+        .post('/teams/addTeamMember')
+        .set('authorization', `Bearer ${token}`)
+        .send({ team_member: 'test3@mail.com' });
+
+      expect(response.status).toEqual(400);
+    });
+
+    test('Should return 400 if team not exist', async () => {
+      await TeamModel.deleteMany({});
+      const response = await request(app)
+        .post('/teams/addTeamMember')
+        .set('authorization', `Bearer ${token}`)
+        .send({ team_member: 'test3@mail.com' });
 
       expect(response.status).toEqual(400);
     });
